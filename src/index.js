5d201381ab9d347747422dee6cab75c584946a71
Damion Shirkey <shirkeydamion@gmail.com>
2026-08-23 12:03:33 -0500

    Add ScreenController for navigation

--- src/index.js
+++ src/index.js
@@ -2,5 +2,30 @@ import "./styles.css";
 import Home from "./screens/home.js";
 import Menu from "./screens/menu.js";
 
-// Home.render();
-Menu.render();
+const ScreenController = (() => {
+    const contentDiv = document.querySelector("#content")
+    const brandingButton = document.querySelector("#brandingButton")
+    const homeButton = document.querySelector("#homeButton");
+    const menuButton = document.querySelector("#menuButton");
+    const contactButton = document.querySelector("#contactButton");
+
+    Home.render();
+
+    const clearContent = () => {
+        contentDiv.textContent = "";
+    }
+
+    const renderHomeScreen = () => {
+        clearContent();
+        Home.render();
+    }
+
+    const renderMenuScreen = () => {
+        clearContent();
+        Menu.render();
+    }
+
+    brandingButton.addEventListener("click", renderHomeScreen);
+    homeButton.addEventListener("click", renderHomeScreen);
+    menuButton.addEventListener("click", renderMenuScreen);
+})();

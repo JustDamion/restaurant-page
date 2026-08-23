28d25577c04f0f9346ac656027cac6e8cb6b1c62
Damion Shirkey <shirkeydamion@gmail.com>
2026-08-23 13:00:59 -0500

    Add active button styling to navbar

--- src/index.js
+++ src/index.js
@@ -10,26 +10,43 @@ const ScreenController = (() => {
     const menuButton = document.querySelector("#menuButton");
     const contactButton = document.querySelector("#contactButton");
 
-    Home.render();
-
     const clearContent = () => {
         contentDiv.textContent = "";
+    }
+
+    const resetButtonStyles = () => {
+        homeButton.removeAttribute("class");
+        homeButton.setAttribute("class", "navbar__item");
+
+        menuButton.removeAttribute("class");
+        menuButton.setAttribute("class", "navbar__item");
+
+        contactButton.removeAttribute("class");
+        contactButton.setAttribute("class", "navbar__item");
     }
 
     const renderHomeScreen = () => {
         clearContent();
         Home.render();
+        resetButtonStyles();
+        homeButton.setAttribute("class", "navbar__item button--active");
     }
 
     const renderMenuScreen = () => {
         clearContent();
         Menu.render();
+        resetButtonStyles();
+        menuButton.setAttribute("class", "navbar__item button--active");
     }
 
     const renderContactScreen = () => {
         clearContent();
         Contact.render();
+        resetButtonStyles();
+        contactButton.setAttribute("class", "navbar__item button--active");
     }
+
+    renderHomeScreen();
 
     brandingButton.addEventListener("click", renderHomeScreen);
     homeButton.addEventListener("click", renderHomeScreen);

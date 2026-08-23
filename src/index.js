814b4844ac11f63c275e45cad509abeecabde3f3
Damion Shirkey <shirkeydamion@gmail.com>
2026-08-23 14:52:17 -0500

    Clean up code in index.js

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import Home from "./screens/home.js";
 import Menu from "./screens/menu.js";
 import Contact from "./screens/contact.js";
 
-const ScreenController = (() => {
+(() => {
     const contentDiv = document.querySelector("#content")
     const brandingButton = document.querySelector("#brandingButton")
     const homeButton = document.querySelector("#homeButton");
@@ -15,13 +15,8 @@ const ScreenController = (() => {
     }
 
     const resetButtonStyles = () => {
-        homeButton.removeAttribute("class");
         homeButton.setAttribute("class", "navbar__item");
-
-        menuButton.removeAttribute("class");
         menuButton.setAttribute("class", "navbar__item");
-
-        contactButton.removeAttribute("class");
         contactButton.setAttribute("class", "navbar__item");
     }
 
@@ -29,21 +24,21 @@ const ScreenController = (() => {
         clearContent();
         Home.render();
         resetButtonStyles();
-        homeButton.setAttribute("class", "navbar__item navbar__item--active");
+        homeButton.classList.add("navbar__item--active");
     }
 
     const renderMenuScreen = () => {
         clearContent();
         Menu.render();
         resetButtonStyles();
-        menuButton.setAttribute("class", "navbar__item navbar__item--active");
+        menuButton.classList.add("navbar__item--active");
     }
 
     const renderContactScreen = () => {
         clearContent();
         Contact.render();
         resetButtonStyles();
-        contactButton.setAttribute("class", "navbar__item navbar__item--active");
+        contactButton.classList.add("navbar__item--active");
     }
 
     renderHomeScreen();
